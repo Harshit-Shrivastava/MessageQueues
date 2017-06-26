8feec1cc6c1487103e6e53c2aeb57a406c781c86
Harshit Shrivastava <hshrivas@indiana.edu>
2017-06-26 17:16:01 -0400

    Initial attemot

---
+++ ReceiveMessageQueue/MyQueue.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Messaging;
+
+namespace ReceiveMessageQueue
+{
+    public class MyQueue
+    {
+        static void Main(string[] args)
+        {
+            MyQueue queue = new MyQueue();
+            while (true)
+            {
+                MyQueue.receiveMessage(); 
+            }
+            Console.Read();
+        }
+
+        static void receiveMessage()
+        {
+            // Connect to a transactional queue on the local computer.
+            MessageQueue myQueue = new
+                MessageQueue(".\\mymsgqueue");
+
+            // Set the formatter.
+            myQueue.Formatter = new XmlMessageFormatter(new Type[]
+                {typeof(String)});
+
+            // Create a transaction.
+            MessageQueueTransaction myTransaction = new
+                MessageQueueTransaction();
+
+            try
+            {
+                // Begin the transaction.
+                myTransaction.Begin();
+
+                // Receive the message. 
+                Message myMessage = myQueue.Receive(myTransaction);
+                String myOrder = (String)myMessage.Body;
+
+                // Display message information.
+                Console.WriteLine(myOrder);
+
+                // Commit the transaction.
+                myTransaction.Commit();
+
+            }
+
+            catch (MessageQueueException e)
+            {
+                // Handle nontransactional queues.
+                if (e.MessageQueueErrorCode ==
+                    MessageQueueErrorCode.TransactionUsage)
+                {
+                    Console.WriteLine("Queue is not transactional.");
+                }
+
+                // Else catch other sources of a MessageQueueException.
+
+                // Roll back the transaction.
+                myTransaction.Abort();
+            }
+
+            // Catch other exceptions as necessary, such as 
+            // InvalidOperationException, thrown when the formatter 
+            // cannot deserialize the message.
+
+            return;
+        }
+    }
+}
---
+++ SendMessageQueue/MyQueue.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Messaging;
+
+namespace SendMessageQueue
+{
+    class MyQueue
+    {
+        static void Main(string[] args)
+        {
+            int m = 1;
+            MyQueue queue = new MyQueue();
+            for (int i = 0; i < 2000; i++)
+            {
+                MyQueue.sendMessage(m);
+                m++; 
+            }
+            Console.Read();
+        }
+
+        static void sendMessage(int m)
+        {
+            // Connect to a queue on the local computer.
+            MessageQueue myQueue = new MessageQueue(".\\mymsgqueue");
+
+            // Send a message to the queue.
+            if (myQueue.Transactional == true)
+            {
+                // Create a transaction.
+                MessageQueueTransaction myTransaction = new
+                    MessageQueueTransaction();
+
+                // Begin the transaction.
+                myTransaction.Begin();
+
+                String message = "My Message Data. " + m;
+
+                Console.WriteLine(  message);
+
+                // Send the message.
+                myQueue.Send(message, myTransaction);
+
+                
+
+                // Commit the transaction.
+                myTransaction.Commit();
+            }
+            else
+            {
+                myQueue.Send("My Message Data.");
+            }
+
+            return;
+        }
+    }
+}
